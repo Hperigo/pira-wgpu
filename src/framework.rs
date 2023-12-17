12ce8742977109d445733448a910d5f78964490a
Henrique Perigo <hperigo@gmail.com>
2023-12-17 12:24:39 -0500

    initial impl

--- src/framework.rs
+++ src/framework.rs
@@ -1,5 +1,7 @@
 use std::time::Instant;
 
+use egui::{FontDefinitions, ViewportId};
+use egui_wgpu::renderer::ScreenDescriptor;
 use winit::{
     dpi::PhysicalSize,
     event::{self, WindowEvent},
@@ -9,7 +11,7 @@ use winit::{
 //use crate::wgpu::{self, factories::render_pass::RenderPassFactory, State};
 use crate::{
     factories::render_pass::RenderPassFactory,
-    state::{Size, State},
+    state::{PerFrameData, Size, State},
 };
 
 pub trait Application: 'static + Sized {
@@ -112,6 +114,18 @@ fn start<E: Application>(
     // INIT APPLICATION
     let mut application = E::init(&state);
 
+    let egui_ctx = egui::Context::default();
+    egui_ctx.set_fonts(FontDefinitions::default());
+    let mut egui_winit_ctx =
+        egui_winit::State::new(ViewportId::ROOT, &window, Some(2.0), Some(1024));
+
+    let mut egui_renderer = egui_wgpu::renderer::Renderer::new(
+        &state.device,
+        wgpu::TextureFormat::Bgra8UnormSrgb,
+        Some(wgpu::TextureFormat::Depth24Plus),
+        4,
+    );
+
     event_loop.run(move |event, _, control_flow| {
         match event {
             winit::event::Event::RedrawRequested(_) => {
@@ -124,18 +138,72 @@ fn start<E: Application>(
 
                 state.delta_time = delta_time.as_millis() as f32;
 
+                let raw_input = egui_winit_ctx.egui_input_mut().take();
+
+                egui_ctx.begin_frame(raw_input);
+                // egui::SidePanel::default().show(&egui_ctx, |ui| {
+                //     ui.label("hey there");
+                // });
+
+                egui::Window::new("Hey there").show(&egui_ctx, |ui| {
+                    ui.label("text");
+                });
+
+                let output = egui_ctx.end_frame();
+                println!("Run!");
+                // let output = egui_ctx.run(raw_input, |ui| {
+                //     egui::CentralPanel::default().show(&ui, |ui| {
+                //         ui.label("Hi there");
+                //     });
+                // });
+                egui_winit_ctx.handle_platform_output(&window, &egui_ctx, output.platform_output);
+
+                println!("Tessellate!");
+                let screen_descriptor = ScreenDescriptor {
+                    size_in_pixels: [window.inner_size().width, window.inner_size().height],
+                    pixels_per_point: 2.0,
+                };
+
+                let primitives = egui_ctx.tessellate(output.shapes, output.pixels_per_point);
+
                 state.render(|ctx, frame_data| {
                     let mut render_pass_factory = RenderPassFactory::new();
-                    render_pass_factory.add_color_atachment(
-                        application.clear_color(),
-                        &frame_data.multisampled_view,
-                        Some(&frame_data.view),
+
+                    let PerFrameData {
+                        encoder,
+                        view,
+                        multisampled_view,
+                    } = frame_data;
+
+                    for (id, image_delta) in &output.textures_delta.set {
+                        egui_renderer.update_texture(&state.device, &state.queue, *id, image_delta);
+                    }
+
+                    for id in output.textures_delta.free {
+                        egui_renderer.free_texture(&id);
+                    }
+
+                    egui_renderer.update_buffers(
+                        &state.device,
+                        &state.queue,
+                        encoder,
+                        &primitives,
+                        &screen_descriptor,
                     );
 
-                    let mut render_pass =
-                        render_pass_factory.get_render_pass(ctx, &mut frame_data.encoder, true);
+                    {
+                        render_pass_factory.add_color_atachment(
+                            application.clear_color(),
+                            &multisampled_view,
+                            Some(&view),
+                        );
+                        let mut render_pass =
+                            render_pass_factory.get_render_pass(ctx, encoder, true);
 
-                    application.render(&state, &mut render_pass);
+                        application.render(&state, &mut render_pass);
+
+                        egui_renderer.render(&mut render_pass, &primitives, &screen_descriptor)
+                    }
                 });
             }
             winit::event::Event::MainEventsCleared => {
@@ -145,6 +213,8 @@ fn start<E: Application>(
                 if matches!(event, WindowEvent::CloseRequested | WindowEvent::Destroyed) {
                     *control_flow = winit::event_loop::ControlFlow::Exit;
                 }
+
+                egui_winit_ctx.on_window_event(&egui_ctx, event);
 
                 if let winit::event::WindowEvent::Resized(physical_size) = event {
                     // state.window_size = Size::new(physical_size.width, physical_size.height);
